42e1bd2b6c97d910121fb1757331fcb830f922ea
RoshanMhjn <mhroshan778@gmail.com>
2024-03-19 19:42:03 +0545

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,11 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
+import dotenv from "dotenv";
+
+dotenv.config();
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -29,5 +33,40 @@ export const signup = async (req, res, next) => {
     res.json({ mesasge: "Signup succesful!" });
   } catch (error) {
     next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      next(errorHandler(404, "Wrong Credentials!"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(400, "Wrong Credentials"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (err) {
+    next(err);
   }
 };
